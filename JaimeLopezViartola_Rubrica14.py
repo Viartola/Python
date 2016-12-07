8b9ccf63554cd39ab1917ed025014be182d4991e
Jaime <jaime.viartola@gmail.com>
2016-12-07 16:52:35 +0100

    Rubrica 1 Signed-off-by: Jaime <jaime.viartola@gmail.com>

--- JaimeLopezViartola_Rubrica14.py
+++ JaimeLopezViartola_Rubrica14.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python
-""" Determina el numero de lineas de 'datos_ficheros' """
+print "*** buscador de palabras dentro de un archivo ***"
+print "*************************************************"
+print ""
 fichero = raw_input("Inserte un fichero:")
 palabra = raw_input("Inserte la palabra a buscar:")
-fd = open(fichero, "r")
+fd = open(fichero)
+lines = fd.readlines()
 nlineas = 0
-for linea in fd:             
-    nlineas += 1
+for linea in lines:
+    palabras = linea.split(' ')
+    for p in palabras:
+        if p==palabra:             
+            nlineas += 1
 fd.close()
 print "Numero de lineas que sale esa palabra:", nlineas
