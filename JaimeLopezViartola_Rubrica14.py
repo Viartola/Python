075e53095d12461a23f36296f7ae445354e8baaa
Jaime <jaime.viartola@gmail.com>
2016-12-09 08:44:25 +0100

    Primera rubrica python Signed-off-by: Jaime <jaime.viartola@gmail.com>

--- JaimeLopezViartola_Rubrica14.py
+++ JaimeLopezViartola_Rubrica14.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python
-print "*** buscador de palabras dentro de un archivo ***"
 print "*************************************************"
-print ""
+print "*** Buscador de palabras dentro de un archivo ***"
+print "*************************************************"
 fichero = raw_input("Inserte un fichero:")
-palabra = raw_input("Inserte la palabra a buscar:")
 fd = open(fichero)
 lines = fd.readlines()
+palabra = raw_input("Inserte la palabra a buscar:")
 nlineas = 0
 for linea in lines:
-    palabras = linea.split(' ')
+    palabras = linea.split()
     for p in palabras:
-        if p==palabra:             
-            nlineas += 1
+       if p==palabra:             
+           nlineas += 1
 fd.close()
 print "Numero de lineas que sale esa palabra:", nlineas
