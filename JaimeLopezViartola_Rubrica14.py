3ef7bba28309559b7c3eb29ee314ba59182d0305
Jaime <jaime.viartola@gmail.com>
2016-12-27 18:15:04 +0100

    Implementación rubrica contador Signed-off-by: Jaime <jaime.viartola@gmail.com>

--- JaimeLopezViartola_Rubrica14.py
+++ JaimeLopezViartola_Rubrica14.py
@@ -1,16 +1,64 @@
 #!/usr/bin/env python
+import sys
 print "*************************************************"
 print "*** Buscador de palabras dentro de un archivo ***"
 print "*************************************************"
-file = raw_input("Inserte un fichero:")
-fd = open(file)
-lines = fd.readlines()
-word = raw_input("Inserte la palabra a buscar:")
-nlines = 0
-for line in lines:
-    words = line.split()
-    for w in words:
-       if w == word:             
-           nlines += 1
-fd.close()
-print "Numero de lineas que sale esa palabra:", nlines
+
+
+def search():  
+    vector.append(word)
+    f = open(file)
+    lines = f.readlines()
+    for line in lines:
+            words = line.split(' ')
+            for p in words:
+                      for x in range(len(vector) ):
+                             if p == vector[x]:
+                                      print(line)                      
+                                     
+def insert():
+    word = raw_input("Inserte una nueva palabra a buscar: ")
+    search()
+    delete()
+    question()
+
+
+def question():
+    question = raw_input("Quieres anadir nueva palabra [s/n]: ")
+    if question == "si" or question == "SI" or question == "S" or question == "s":
+         insert()
+    elif question == "no" or question == "NO" or question == "N" or question == "n" :
+        print "************************"
+        print "*** Hasta la proxima ***"
+        print "************************"
+        sys.exit(0)
+    else:
+        print "**********************************"
+        print "*** Inserte un caracter valido ***"
+        print "**********************************"
+        question2()
+
+def question2():
+    question = raw_input("Quieres anadir nueva palabra [s/n]: ")
+    if question == "si" or question == "SI" or question == "S" or question == "s":
+         insert()
+    elif question == "no" or question == "NO" or question == "N" or question == "n" :
+        print "************************"
+        print "*** Hasta la proxima ***"
+        print "************************"
+        sys.exit(0)
+    else:
+        print "**********************************"
+        print "*** Inserte un caracter valido ***"
+        print "**********************************"
+        question()
+        
+def delete():
+    vector.remove(word)
+               
+file = raw_input("Inserte un fichero: ")
+word = raw_input("Inserte la palabra a buscar: ")
+vector = [ ]
+search()
+delete()
+question()
